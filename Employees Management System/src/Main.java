e7a3284ccaf7a8736a8ec0dbb0e72f6b043a9f1c
之之 <1591217579@qq.com>
2024-12-06 20:52:57 +0800

    Update

--- Employees Management System/src/Main.java
+++ Employees Management System/src/Main.java
@@ -1,7 +1,8 @@
 import Staff.Staff;
-
+import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 //TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
 // 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
@@ -41,14 +42,32 @@ public class Main {
             if (staff.get(i).getGender()==gender&&staff.get(i).getAge()<maxAge&&staff.get(i).getAge()>minAge&&staff.get(i).getIncome()==income){
                 requiredStaff.add(staff.get(i));
             }else {
-                System.out.println("no required employee");
+                System.out.println("no required employee");//存在问题，使用empty
             }
         }
-
-
-
     }
 
+    //未写完
+    public void searchStaffInformation(String name){
+        int temp=0,index=-1;
+        for (int i=0;i<staff.size();i++){
+            if (staff.get(i).getName()==name)
+            {
+                index=i;
+                temp+=1;
+            }
+        }
+        if(temp==0)System.out.println("Your search did not match any staff");
+        if(temp==1)
+        {
+            ;
+        }
+        if(temp>=2)
+        {
+            System.out.println("Your search matches "+ temp +" staff");;
+
+        }
+    }//按名字查询
 
     public static void main(String[] args) {
 
